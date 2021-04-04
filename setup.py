411a2437a538b0491abf0e66ed54c7c482033746
Michael Groenewegen van der Weijden <michael@fragrant-chios.com>
2021-04-04 12:27:59 +0200

    project setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cs-algorithms", # Replace with your own username
+    version="0.0.1",
+    author="Michael Groenewegen van der Weijden",
+    author_email="",
+    description="Sorting Algorithms & Data Structures",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/michael-gvdw/cs-algorithms.git",
+    project_urls={
+        "GitHub": "https://github.com/michael-gvdw/cs-algorithms.git",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
---
+++ src/cs-algorithms/data_structures.py
@@ -0,0 +1,16 @@
+
+
+class Queue(object):
+
+    def __init__(self):
+        return None
+
+class Stack():
+
+    def __init__(self):
+        return None
+
+class BinaryTree():
+
+    def __init__(self):
+        return None
